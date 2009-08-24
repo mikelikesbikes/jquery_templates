21678f3bcb241e1000e57a9a220648c11b94b289
Mike Busch <mike@toobla.com>
2009-08-24 19:50:39 -0400

    made separator into a configurable param

--- jquery-templates.js
+++ jquery-templates.js
@@ -1,9 +1,16 @@
 (function($) {
   $.template = function(str, params) {
-    return str.replace(/__(.+?)__/g,
+    var begin_sep = $.template.defaults.begin_sep
+    var end_sep = $.template.defaults.end_sep
+    regex = new RegExp(begin_sep + "(.+?)" + end_sep, "g");
+    return str.replace(regex,
       function (a, b) {
         var r = params[b];
         return typeof r === 'string' || typeof r === 'number' ? r : "";
       });
+  };
+  $.template.defaults = {
+    "begin_sep" : "__",
+    "end_sep": "__"
   };
 })(jQuery);
