7ddb24ce410ceceefdd616b819b025e578cd77db
Mike Busch <mike@toobla.com>
2009-08-24 20:05:50 -0400

    added jQuery options/settings extension pattern

--- jquery-templates.js
+++ jquery-templates.js
@@ -1,14 +1,15 @@
 (function($) {
-  $.template = function(str, params) {
-    var begin_sep = $.template.defaults.begin_sep
-    var end_sep = $.template.defaults.end_sep
-    regex = new RegExp(begin_sep + "(.+?)" + end_sep, "g");
+  $.template = function(str, params, options) {
+    var settings = $.extend({}, $.template.defaults, options);
+
+    regex = new RegExp(settings.begin_sep + "(.+?)" + settings.end_sep, "g");
     return str.replace(regex,
       function (a, b) {
         var r = params[b];
         return typeof r === 'string' || typeof r === 'number' ? r : "";
       });
   };
+
   $.template.defaults = {
     "begin_sep" : "__",
     "end_sep": "__"
